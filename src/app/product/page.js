e4b8b81bfd653106fec7fde7c3c4df50e2ce086b
bishwa <108havocx108@gmail.com>
2024-07-12 22:22:43 +0545

    practising react

--- src/app/product/page.js
+++ src/app/product/page.js
@@ -1,28 +1,56 @@
-import { name } from 'ejs'
+'use client'
+// import { name } from 'ejs'
 import React from 'react'
+import Link from 'next/link'
 
-const page = () => {
-    const app ='uProd'
-    const productList = [
-        {name:'football', price:1500, quantity: 5, imgLink: 'https://purepng.com/public/uploads/large/purepng.com-golden-footballobjectsgolden-footballballsoccerobjectgoldgoldensportfootball-631522323197a79p6.png' },
-        {name:'bat', price:20000 , quantity:8, imgLink: 'https://m.media-amazon.com/images/I/317VTY6RngL._AC_UF1000,1000_QL80_.jpg'},
-        {name:'helmet', price:6000, quantity:2, imgLink: 'https://5.imimg.com/data5/SELLER/Default/2021/4/RF/MS/HN/74206329/vega-cliff-full-face-bike-helmet-500x500.jpg'},
-    ]
+
+// const page = () => {
+//     const app ='uProd'
+//     const productList = [
+//         {id: 7, name:'football', price:1500, quantity: 5, imgLink: 'https://purepng.com/public/uploads/large/purepng.com-golden-footballobjectsgolden-footballballsoccerobjectgoldgoldensportfootball-631522323197a79p6.png' },
+//         {id: 10,name:'bat', price:20000 , quantity:8, imgLink: 'https://m.media-amazon.com/images/I/317VTY6RngL._AC_UF1000,1000_QL80_.jpg'},
+//         {id: 30,name:'helmet', price:6000, quantity:2, imgLink: 'https://5.imimg.com/data5/SELLER/Default/2021/4/RF/MS/HN/74206329/vega-cliff-full-face-bike-helmet-500x500.jpg'},
+//     ]
+// 	return (
+// 		<div>
+// 			<div style={{display:"flex"}}>
+// 				{productList.map((item,id) => {
+// 				 return <Link href={`/product/${item.id}`}>
+// 				    <div className='card'>
+// 					 <img style= {{width:"90px", height:"80px"}} src={item.imgLink}></img>
+// 					 {item.name}
+// 					 <p>{item.price}</p>
+
+// 				    </div> </Link>
+// 			    })}
+// 			</div>
+
+// 		</div>
+
+// 	)
+// }
+
+const test = () => {
+	const handleClick = () =>{
+		console.log("this is from test")
+	}
+
 	return (
 		<div>
-			<div style={{display:"flex"}}>
-				{productList.map((item,id) => {
-				 return <div className='card'>
-					<img style= {{width:"90px", height:"80px"}} src={item.imgLink}></img>
-					{item.name}
-					<p>{item.price}</p>
-				</div>
-			})}
-			</div>
-
+		 <input/>
+		 <Cart chocolate = "kitkat" callMe ={handleClick} />
 		</div>
-
 	)
 }
 
-export default page
+const Cart = (props) => {
+  return (
+	<div>
+	 <button onClick={props.callMe}>ClickME</button>
+	</div>
+  )
+}
+
+
+
+export default test
