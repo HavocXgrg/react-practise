e2dfaf58365c56dc7cb4d5abb6e75db2f01d3ed7
bishwa <108havocx108@gmail.com>
2024-07-21 16:13:52 +0545

    refactored code by implementing DRY principle, also the use of key prop

--- src/app/reaction_btn/page.js
+++ src/app/reaction_btn/page.js
@@ -10,41 +10,41 @@ const page = () => {
     const [reactionType, setReactionType] = useState(null)
 
     const HandleReaction = (action) =>{
-        if(reactionType === action){
-            setReactionType('')
-        } else{
-            setReactionType(action)
-        }
+        // console.log(reactionType, action);
+        setReactionType(reactionType === action ? '' : action); //ternary operators
+
     }
+    const reactions = [
+        {type: 'like', icon:<GrLike size={'2rem'} color='blue' /> },
+        {type: 'angry', icon:<LuAngry size={'2rem'}color='red' /> },
+        {type: 'sad', icon:<TfiFaceSad size={'2rem'}color='orange' /> },
+        {type: 'love', icon:<FiHeart size={'2rem'}color='red' /> },
+        {type: null, icon:<GrLike size={'2rem'} /> }
+    ]
 
     const ReactionDiv = () => {
         return (
-            <div className="reactionBtn">
-                 <button  onMouseEnter={()=> setReactionOpen(true)} onClick={()=> HandleReaction('like')}><GrLike size={'2rem'} color='blue' /></button>
-                 <button  onMouseEnter={()=> setReactionOpen(true)} onClick={()=> HandleReaction('angry')}><LuAngry size={'2rem'}color='red' /></button>
-                 <button  onMouseEnter={()=> setReactionOpen(true)}onClick={()=> HandleReaction('love')}><FiHeart size={'2rem'}color='red' /></button>
-                 <button  onMouseEnter={()=> setReactionOpen(true)}onClick={()=> HandleReaction('sad')}><TfiFaceSad size={'2rem'}color='orange' /></button>
+            <div>
+                {reactions.map((item) => {
+                    if(item.type)
+                   return(
+                            <button //key for no key prop warning
+                                key={item.type} onMouseEnter={() => setReactionOpen(true)} onClick={() => HandleReaction(item.type)}>
+                                {item.icon}
+                            </button>
+                        )
+                 })}
             </div>
         )
     }
     const ReactionBtn = () => {
-        if(reactionType === 'angry'){
-            return(
-                <button onClick={()=> HandleReaction('angry')} onMouseEnter={()=> setReactionOpen(true)}><LuAngry color={reactionType && 'red'} size={'2rem'}/></button>
-            )
-        } else if(reactionType === 'love'){
-            return (
-                <button onClick={()=> HandleReaction('love')} onMouseEnter={()=> setReactionOpen(true)}><FiHeart color={reactionType && 'red'}  size={'2rem'}/></button>
-            )
-        } else if(reactionType === 'sad'){
-            return(
-                <button onClick={()=> HandleReaction('sad')} onMouseEnter={()=> setReactionOpen(true)}><TfiFaceSad color={reactionType && 'orange'} size={'2rem'}/></button>
-            )
-        } else{
-            return ( 
-                <button onClick={()=> HandleReaction('like')} onMouseEnter={()=> setReactionOpen(true)}><GrLike color={reactionType && 'blue'} size={'2rem'}/></button>
-                )
-        }
+        const selectedItem = reactions.find(item => item.type === reactionType)
+        return(
+            <button  onMouseEnter={()=> setReactionOpen(true)} 
+                onClick={()=> HandleReaction(selectedItem?.type || '')}>
+                {selectedItem ? selectedItem.icon : <GrLike size={'2rem'} />} 
+            </button> 
+        )
     }
   return (
 
